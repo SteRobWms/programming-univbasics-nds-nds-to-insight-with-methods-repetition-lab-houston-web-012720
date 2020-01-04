a03a6a766acbd5cc5ad0e735ce43332a6daca284
Stephen Williams <stephen.r.williams.87@gmail.com>
2020-01-04 23:03:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,12 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  d_list = []
+  index = 0
+  while index < source.length do
+    d_list << source[index][:name]
+    index += 1
+  end
 end
 
 def total_gross(source)
